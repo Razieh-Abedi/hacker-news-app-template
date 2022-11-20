5cbec08f959b1da32bb977e7490c718cc1712c0f
Razieh Abedi <raziyeabedi@gmail.com>
2022-11-20 15:24:34 +0800

    created the app

---
+++ src/context.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useReducer, useContext } from "react";
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
